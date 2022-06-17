d1f5655402ea3e324d42910a35d517ffd856ce77
Zoheir Kabuli <zoheyrk@gmail.com>
2022-06-17 22:03:39 +0430

    add and fix swiper

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,3 +2,22 @@
 feather.replace();
 
 // carousel
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    
+    // when window width is >= 640px
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+  },
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
